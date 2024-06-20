1174b9fdb935769f6581b1e54f70e20b83bf64d1
IndraneelVarma <manthenaindraneel@gmail.com>
2024-06-20 21:34:45 +0530

    Initial Commit

---
+++ weatherApp/Components/WeatherButton.swift
@@ -0,0 +1,31 @@
+//
+//  MyButton.swift
+//  weatherApp
+//
+//  Created by Indraneel Varma on 17/06/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    @Binding var isNight: Bool
+    var body: some View {
+        Button{
+            print(isNight)
+            isNight.toggle()
+            } label: {
+            Text(title)
+                .frame(width: 250, height: 50)
+                .foregroundColor(textColor)
+                .background(backgroundColor)
+                .font(.system(size: 20,weight: .bold))
+                .cornerRadius(10)
+        }
+    }
+}
+
+
---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Indraneel Varma on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
